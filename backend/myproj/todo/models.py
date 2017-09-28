70ff0e0198348f07b8e54a981ebb6f272fb7fa9b
jiaqi0919 <694233783@com>
2017-09-28 13:48:09 +0800

    'fix'

--- backend/myproj/todo/models.py
+++ backend/myproj/todo/models.py
@@ -1,11 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.db import models
+
+# Create your models here.
 
 from django.db import models
+
 
 # Create your models here.
 class Item(models.Model):
     content = models.TextField(max_length=200)
     status = models.IntegerField(default=0)
     priority = models.IntegerField(default=0)
-    expirate_date = models.DateField()
+    expire_date = models.DateField()
---
+++ backend/myproj/todo/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Item
+from .serializers import ItemSerializer
+# Create your views here.
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
