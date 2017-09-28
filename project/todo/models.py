17c0ecc081119712e64ab5b74d38695a5dfcd200
jasonpanacea <f6434382@126.com>
2017-09-28 09:20:28 +0800

    field

--- project/todo/models.py
+++ project/todo/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    content = models.TextField(max_length=200)
+    status = models.IntegerField(default=0)
+    priority = models.IntegerField(default=0)
+    expirate_date = models.DateField()
