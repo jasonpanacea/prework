61e8d8dcba72c61fb4ebbbbcc2bf588aaf9c2e43
jiaqi0919 <694233783@com>
2017-09-29 10:09:10 +0800

    add field

---
+++ backend/myproj/todo/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Item',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('content', models.TextField(max_length=200)),
+                ('status', models.IntegerField(default=0)),
+                ('priority', models.IntegerField(default=0)),
+                ('expire_date', models.DateField()),
+            ],
+        ),
+    ]
--- backend/myproj/todo/models.py
+++ backend/myproj/todo/models.py
@@ -3,11 +3,13 @@ from django.db import models
 # Create your models here.
 
 from django.db import models
-
+from django.utils import timezone
 
 # Create your models here.
 class Item(models.Model):
     content = models.TextField(max_length=200)
     status = models.IntegerField(default=0)
     priority = models.IntegerField(default=0)
-    expire_date = models.DateField()
+    expire_date = models.DateField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True)
+    updated_at = models.DateTimeField(auto_now=True,blank=True)
